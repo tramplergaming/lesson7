40d39dcd6de93e7f0c336afc6e22305f17b8dc57
Top <Top@8-19>
2024-10-25 18:37:15 +0500

    eshkere

--- lesson7/hand.hpp
+++ lesson7/hand.hpp
@@ -1,1 +1,17 @@
 #pragma once
+#ifndef HAND_HPP
+#define HAND_HPP
+
+#include <vector>
+#include "card.hpp"
+
+namespace CardGame{
+
+	class Hand {
+	public:
+		//нужно добавить методы, подходящие для карточной руки
+	private:
+		std::vector<Card> cards_;
+	};
+}
+#endif 
